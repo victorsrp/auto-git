340d4b25024538e37b5461f02e6447f786c14f0c
Victor Pereira <victorsrp@outlook.com>
2025-09-25 00:42:48 -0300

    Add condition if

--- auto-git.sh
+++ auto-git.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-selected=$(git branch | fzf \
+selected=$(git branch | fzf +m \
     --height 40% \
     --layout reverse \
     --border \
     --preview \
-        'git log --oneline $(echo {} | tr -d "* ")')
+        'git log --oneline $(echo {} | tr -d "* ")' \
+    --color bg:#222222,preview-bg:#333333)
+
+if [ $? -eq 130 ]; then
+    echo "Exiting..."
+    exit 1
+fi
 
 selected=$(echo $selected | tr -d "* ")
 
