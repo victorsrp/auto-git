aa17e98de84931d7bb5ba7cffd0e6d80b1cb33e6
Victor Pereira <victorsrp@outlook.com>
2025-09-25 00:30:32 -0300

    Add git log with logic shell and git

--- auto-git.sh
+++ auto-git.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-hello="Hello World!"
-echo "$hello" 
+selected=$(git branch | fzf \
+    --height 40% \
+    --layout reverse \
+    --border \
+    --preview \
+        'git log --oneline $(echo {} | tr -d "* ")')
+
+selected=$(echo $selected | tr -d "* ")
+
+echo "$selected"
+
+git switch "$selected"
